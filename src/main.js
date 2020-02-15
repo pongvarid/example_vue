e4780c5fa6903ec008948cf0542b963d226b3338
พงษ์วริษฐ์ มณีวรรณ์ <pongvarid@gmail.com>
2020-02-15 23:24:29 +0700

    init project

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router' 
+import '@/assets/css/base.css'; 
+import '@/assets/css/custom.css'; 
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: function (h) { return h(App) }
+}).$mount('#app')
